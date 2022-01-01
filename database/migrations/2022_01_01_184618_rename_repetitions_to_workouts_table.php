55e3353419dc3bda41ac10fc3c3208153c89820c
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2022-01-01 23:15:45 +0100

    fix: workouts migration
    (cherry picked from commit ca8060d1a687fc2a3162411517c462bdd19f0405)

--- database/migrations/2022_01_01_184618_rename_repetitions_to_workouts_table.php
+++ database/migrations/2022_01_01_184618_rename_repetitions_to_workouts_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameWorkoutsToWorkoutsTable extends Migration
+class RenameRepetitionsToWorkoutsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class RenameWorkoutsToWorkoutsTable extends Migration
      */
     public function up()
     {
-        Schema::rename('workouts', 'workouts');
+        Schema::rename('repetitions', 'workouts');
         Schema::table('workouts', function (Blueprint $table) {
             $table->dropForeign('workouts_user_id_foreign');
             $table->dropForeign('workouts_exercise_id_foreign');
@@ -41,18 +41,18 @@ class RenameWorkoutsToWorkoutsTable extends Migration
      */
     public function down()
     {
-        Schema::rename('workouts', 'workouts');
-        Schema::table('workouts', function (Blueprint $table) {
+        Schema::rename('workouts', 'repetitions');
+        Schema::table('repetitions', function (Blueprint $table) {
             $table->dropForeign('workouts_user_id_foreign');
             $table->dropForeign('workouts_exercise_id_foreign');
 
-            $table->foreign('exercise_id', 'workouts_exercise_id_foreign')
+            $table->foreign('exercise_id', 'repetitions_exercise_id_foreign')
                 ->references('id')
                 ->on('exercises')
                 ->cascadeOnDelete()
                 ->cascadeOnUpdate();
 
-            $table->foreign('user_id', 'workouts_user_id_foreign')
+            $table->foreign('user_id', 'repetitions_user_id_foreign')
                 ->references('id')
                 ->on('users')
                 ->cascadeOnDelete()
